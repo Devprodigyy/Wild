9947e7a717bfe66d83d91dfe5f8bf9bda0093cb8
kunalwatulkar <kunalwatulkar1@gmail.com>
2025-05-09 22:32:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.from("nav",{
+    opacity:0,
+    delay:3,
+    duration:1,
+     ease: "power1.inOut"
+},"a")
+
+gsap.from("footer",{
+    opacity:0,
+    delay:3,
+    duration:1,
+     ease: "power1.inOut"
+},"a")
+
+gsap.from(".line",{
+    width:"0%",
+    delay:3,
+    duration:1,
+     ease: "power1.inOut"
+},"a")
+
+gsap.from(".imgcontianer",{
+    width:0,
+    height:"200%",
+    duration:1,
+    delay:2,
+      ease: "power1.inOut"
+})
